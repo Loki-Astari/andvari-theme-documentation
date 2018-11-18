a5d39d11e69095e023cd84a0a55568d7b5613d4c
Loki Astari <Loki.Astari@gmail.com>
2018-11-18 13:50:17 -0800

    Display Template Info for classes

--- script/buildDisplay.js
+++ script/buildDisplay.js
@@ -1,6 +1,12 @@
 
 function buildDisplay(text) {
-    return text ? text.replace("-L", "&lt;").replace("-R", "&gt;").replace(/-/g, " ") : "";
+    return !text ? ""
+                :text
+                    .replace("<",  "&lt;")
+                    .replace("-L", "&lt;")
+                    .replace(">",  "&gt;")
+                    .replace("-R", "&gt;")
+                    .replace(/-/g, " ");
 }
 
 module.exports = buildDisplay;
